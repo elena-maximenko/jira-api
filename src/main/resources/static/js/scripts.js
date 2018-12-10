6c6115d0bd14daaf7d2c3161134ba5cc34847b6d
elena-maximenko <amely.honey@gmail.com>
2018-12-10 20:03:27 +0200

    Removed useless logging.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,6 +1,5 @@
 function hey() {
     let string = $("#formInput").val();
-    console.log(string);
 
     let xmlHttpRequest = new XMLHttpRequest();
     xmlHttpRequest.open("POST", "http://localhost:8080/example", true);
